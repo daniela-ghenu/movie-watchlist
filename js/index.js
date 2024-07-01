22536cc4cdb25993a13908c305946e34c3bd64f3
Andreea-Daniela Ghenu <andreea.ghenu@welink.ro>
2024-07-01 16:11:55 +0300

    Restructure project (in progress)

--- js/index.js
+++
@@ -1,3 +0,0 @@
-import "./movie-search.js";
-import "./handle-modal-info.js";
-import "./manage-watchlist.js";
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./modules/movie-search.js";
+import "./modules/handle-modal-info.js";
+import "./modules/manage-watchlist.js";
