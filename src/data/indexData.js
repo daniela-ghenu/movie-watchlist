681bbe6c2b5f4d3f6ee02890c07011b7ad9d5e1f
daniela-ghenu <daniela.andreea.ghenu@gmail.com>
2024-08-02 09:55:37 +0300

    Improvements to hero font size, spacing and alignment

--- src/data/indexData.js
+++ src/data/indexData.js
@@ -3,7 +3,7 @@ export default {
   description: "Search your favorite movies, view information about them and add them to a personalized watchlist.",
   hero: { 
     sectionId: "search-page",
-    title: "Welcome to Movie&nbsp;Search&nbsp;App",
+    title: "Welcome to Movie Search&nbsp;App",
     subtitle: "Here you can find and save your favorite movies."
   }
 }
